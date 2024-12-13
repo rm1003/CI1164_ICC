e200d6daedbe993e2491ea301352565a77680b84
Ruibin <ruibin2018@gmail.com>
2024-12-13 13:05:29 -0300

    Delete files

--- EP_03_Ajuste_de_Curvas/v2/ajustePol.c
+++ EP_03_Ajuste_de_Curvas/v2/ajustePol.c
@@ -8,38 +8,45 @@
 #include "utils.h"
 
 #define UNROLL_LINES 4
+#define BLOCKING 4
 /////////////////////////////////////////////////////////////////////////////////////
 //   AJUSTE DE CURVAS
 /////////////////////////////////////////////////////////////////////////////////////
 
 // OK
 // Add restrict para poder distinguir as matrizes
-void montaSL(double **restrict A, double *restrict b, int n, long long int p, double *restrict x, double *restrict y) {       
+void montaSL(double **restrict A, double *restrict b, int n, long long int p, double *restrict x, double *restrict y)
+{
     // Antecipa calculo do pow para reutilização em vez de calcular novamente toda vez
     // max_power = para valor max na matriz
     long long int max_power = 2 * n - 1;
-    double *powers = (double*) malloc((max_power) * sizeof(double));
-    for (long long int i = 0; i < max_power; ++i) {
+    double *powers = (double *)malloc((max_power) * sizeof(double));
+    for (long long int i = 0; i < max_power; ++i)
+    {
         powers[i] = 0.0;
     }
 
-    for (long long int i = 0; i < max_power; ++i) {
-        for (long long int j = 0; j < p; ++j) {
+    for (long long int i = 0; i < max_power; ++i)
+    {
+        for (long long int j = 0; j < p; ++j)
+        {
             powers[i] += pow(x[j], i);
         }
     }
 
-    for (long long int i = 0; i < n; ++i) {
+    for (long long int i = 0; i < n; ++i)
+    {
         b[i] = 0.0;
-        for (long long int j = 0; j < n; ++j) {
+        for (long long int j = 0; j < n; ++j)
+        {
             A[i][j] = powers[i + j];
         }
-        for (long long int k = 0; k < p; ++k) {
+        for (long long int k = 0; k < p; ++k)
+        {
             b[i] += pow(x[k], i) * y[k];
         }
     }
 }
-
 
 void eliminacaoGauss(double **restrict A, double *restrict b, int n)
 {
@@ -72,11 +79,13 @@ void eliminacaoGauss(double **restrict A, double *restrict b, int n)
     }
 }
 
-void retrossubs(double **restrict A, double *restrict b, double *restrict x, int n) {
-
-    for (int i = n - 1; i >= 0; --i) {
+void retrossubs(double **restrict A, double *restrict b, double *restrict x, int n)
+{
+    for (long long int i = n - 1; i >= 0; --i)
+    {
         x[i] = b[i];
-        for (int j = i + 1; j < n; ++j) {
+        for (long long int j = i + 1; j < n; ++j)
+        {
             x[i] -= A[i][j] * x[j];
         }
         x[i] /= A[i][i];
